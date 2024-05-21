fda25329805a61bfd1a7582b202e09cf5915ed90
Leechoshin <158303213+Leechoshin@users.noreply.github.com>
2024-05-21 19:10:59 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int person, time, sum = 0,all=0,count = 0;
+	vector<int> v;
+	cin >> person;
+	for (int i = 0; i < person; i++) {
+		cin >> time;
+		v.push_back(time);
+	}
+
+	sort(v.begin(), v.end());
+	while (count != v.size()) {
+		for (int i = 0; i <= count; i++) {
+			sum += v[i];
+		}
+		count++;
+	}
+	cout << sum;
+}
