3d1879e4096696c86248a7202a4983b6acb74b77
Leechoshin <158303213+Leechoshin@users.noreply.github.com>
2024-10-02 17:21:47 +0900

    [Silver III] Title: 통계학, Time: 64 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -46,6 +46,7 @@ int main() {
 	cout << v[num / 2] << '\n';
 
 	// 최빈값 구하기
+	// 최빈값은 구글링을 통해 참고했습니다.
 	int count[8001] = { 0 };
 	for (int i = 0; i < num; i++) {
 		if (v[i] >= 0) {
@@ -56,33 +57,37 @@ int main() {
 		}
 	} // 벡터에 들어있는 수들의 개수를 저장해놓은 count 배열
 
-	int max = 0;
+	int max = 0; // count 배열에서 가장 큰 최빈값을 찾기 위해 max 변수 설정
+	// 배열을 돌면서 max 값을 찾기
 	for (int i = 0; i < 8001; i++) {
 		if (max < count[i]) {
 			max = count[i];
 		}
 	}
-	vector<int> Find_Mode;
+	vector<int> Find_Mode; // 어떤 수가 max 최빈값과 동일한지 저장하는 Find_Mode 벡터
 	for (int i = 0; i < 8001; i++) {
 		if (max == count[i]) {
-			if (i <= 4000) {
+			if (i <= 4000) { // count[i]의 수가 양수일 경우
 				Find_Mode.push_back(i);
 			}
-			else {
+			else { // count[i]의 수가 음수일 경우
 				Find_Mode.push_back((i - 4000) * (-1));
 			}
 		}
 	}
-	sort(Find_Mode.begin(), Find_Mode.end());
-
+	sort(Find_Mode.begin(), Find_Mode.end()); // Find_Mode 벡터를 정렬
+	// Find_Mode 벡터를 정렬하게 되면 가장 큰 최빈값을 가지는 값들이 내림차순으로 정렬됨
+	// 가장 큰 최빈값을 가지는 값이 하나인 경우
 	if (Find_Mode.size() == 1) {
-		cout << Find_Mode[0] << '\n';
+		cout << Find_Mode[0] << '\n'; // Find_Mode 벡터의 index가 0인 값 출력
 	}
+	// 가장 큰 최빈값을 가지는 값이 2개 이상인 경우
 	else {
-		cout << Find_Mode[1] << '\n';
+		cout << Find_Mode[1] << '\n'; // Find_Mode 벡터의 index가 1인 값 출력
 	}
 	
 
 	// 범위 구하기
 	cout << v[num - 1] - v[0];
+
 }
