bef30dece44ce1bbd397a8fd560665151ad4aab8
Leechoshin <158303213+Leechoshin@users.noreply.github.com>
2025-03-31 18:40:07 +0900

    [level 1] Title: 내적, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0;
+    int size = a.size();
+    for(int i=0;i<size;i++){
+        answer+=a[i]*b[i];
+    }
+    
+    return answer;
+}
